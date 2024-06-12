4576b46948ed1cad028e0bfb71c169b5f85b91d6
Reggie <reggie@Reggies-MBP.fios-router.home>
2024-06-12 03:04:22 -0400

    nutrition app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React from "react"; 
+import NutritionMeter from "./NutritionMeter"; 
+  
+function App() { 
+  return ( 
+    <div className="bg-gray-100 min-h-screen"> 
+      <NutritionMeter /> 
+    </div> 
+  ); 
+} 
+  
 export default App;
